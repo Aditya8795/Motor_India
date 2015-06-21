1e5169f6fa0317810941e2cd90dbca3af6ccb1e9
Aditya Prasad <adityaprasadiskool@gmail.com>
2015-06-21 22:15:07 +0530

    Working on GCM - server side
    Well, I am sorry to report that in the last commit I had only perfected
    client side.
    
    GCMregister library required a different server implementation than
    the GCM API, I used the (new and improved) Postman to send a POST as
    the GCM API server guide explained and have got the notification in
    my mobile.
    
    The last commit's false positive was due to the fact that I mistook the
    other motorindia's notification as this one's.
    
    Well I better get this wrapped up. cheers
    
    Signed-off-by: Aditya Prasad <adityaprasadiskool@gmail.com>

--- app/src/main/java/in/motorindiaonline/motorindia/Utilities/CommonData.java
+++ app/src/main/java/in/motorindiaonline/motorindia/Utilities/CommonData.java
@@ -3,15 +3,15 @@ package in.motorindiaonline.motorindia.Utilities;
 public class CommonData {
     /*
 Server API key:
-AIzaSyCsKwKuuLBbq-0CEZSC5wmJDyXpDHqRWU0
+AIzaSyCrWAt9mpv_4_SeRLA3w7r6gdGC43VZ2cc
 Sender ID:
-544864356150
+550448643561
     */
 
     // give your server registration url here gcm_server_php/register.php
-    public static final String SERVER_URL = "http://www.motorindiaonline.in/gcm/";
+    public static final String SERVER_URL = "http://4e25eec.ngrok.com/gcm/";
     // To Obtain a registration token
-    public static final String SENDER_ID = "544864356150";
+    public static final String SENDER_ID = "550448643561";
     // Amount of time (in milliseconds) that the splash screen should be shown
     public static final int SPLASH_SCREEN_DELAY = 1500;
 }
